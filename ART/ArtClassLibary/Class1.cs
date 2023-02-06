d442e7ad5def09e7763032467c0ce8cf115ddea4
Artur <97942839+artur4real@users.noreply.github.com>
2023-02-06 16:40:18 +0300

    Add files via upload

---
+++ ART/ArtClassLibary/Class1.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kon
+{
+    public class Class1
+    {
+        string hourse; // конь 
+        int currentlevelHP; // текущее здоровье 
+        int speed; // скорость 
+        int max_level_HP; // максимальный уровень здоровья
+        int nickname; // кличка 
+        int damage; // урон 
+        
+
+        public int СurrentlevelHP
+        {
+            get { return currentlevelHP; } // возвращает значение 
+            set { currentlevelHP = value; } // задает значение 
+
+        }
+
+        public int Speed
+        {
+            get { return speed; } // возвращает значение 
+            set { speed = value; } // задает значение 
+
+        }
+        public int Max_level_HP
+        {
+            get { return max_level_HP; } // возвращает значение 
+            set { max_level_HP = value; } // задает значение 
+
+
+
+        }
+        
+
+        public int Nickname
+        {
+            get { return damage; } // возвращает значение 
+            set { damage = value; } // задает значение 
+
+
+
+        }
+
+                                                                             
+        
+ 
+                                                                                                             
+
+
+
+
+
+    }
+}
